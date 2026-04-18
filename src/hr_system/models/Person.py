d3765d00cba959002c1f1d961fce7704074e480f
successdevop <successraphael28@gmail.com>
2026-04-18 10:56:34 +0100

    build out the parent class Person from which every employee would inherit from

--- src/hr_system/models/Person.py
+++ src/hr_system/models/Person.py
@@ -1,14 +1,41 @@
-class _Person:
-    def __int__(self, name: str, email: str, age: int, nin: str, state_of_origin: str):
-        self.__name = name
-        self.__email = email
-        self.__age = age
-        self.__NIN = nin
-        self.__origin = state_of_origin
+from ..utils.utils import Utils
+
+
+class Person:
+    def __init__(self, name: str, email: str, age: int, state_of_origin: str):
+        self._name = Utils.validate_name(name)
+        self._email = Utils.validate_email(email)
+        self._age = Utils.validate_age(age)
+        self._origin = Utils.validate_name(state_of_origin)
 
     @property
     def name(self):
-        return self.__name
+        return self._name
+
     @name.setter
     def name(self, name):
-        new_name =
+        self._name = Utils.validate_name(name)
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, email):
+        self._email = Utils.validate_email(email)
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, age):
+        self._age = Utils.validate_age(age)
+
+    @property
+    def origin(self):
+        return self._origin
+
+    @origin.setter
+    def origin(self, state_of_origin):
+        self._origin = Utils.validate_name(state_of_origin)
--- src/hr_system/utils/utils.py
+++ src/hr_system/utils/utils.py
@@ -5,29 +5,29 @@ import re
 class Utils:
     """ this class provides the functions/methods that serves as helper function to our application"""
     @staticmethod
-    def generate_nin(employee_database):
+    def generate_nin(employee_database: list) -> str:
         while True:
             nin = "".join(str(random.randint(1, 9)) for _ in range(10))
             if not any(person.nin == nin for person in employee_database):
                 return nin
 
     @staticmethod
-    def validate_name(name):
-        name = name.strip()
+    def validate_name(name: str) -> str:
+        name = name.strip().title()
         if name and len(name) >= 3:
             return name
         else:
             print("Name value cannot be empty and must be at-least 3 characters")
 
     @staticmethod
-    def validate_email(email):
+    def validate_email(email: str) -> str:
         pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
         if re.match(pattern, email):
             return email
         print("Invalid email")
 
     @staticmethod
-    def validate_age(age):
+    def validate_age(age: int) -> int:
         if isinstance(age, int):
             if age >= 18:
                 return age
@@ -35,11 +35,3 @@ class Utils:
                 print("You must be at-least 18 years")
         else:
             print("Invalid age value")
-
-
-# util_ch = Utils()
-# # util_ch.generate_nin(["1234567890", "2345678901", "3456789012", "4567890123", "5678901234"])
-# util_ch.validate_age("32")
-# util_ch.validate_name("er")
-# print(util_ch.validate_email("success.@gmail.com"))
-
