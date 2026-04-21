da58cce69baac7df263004c1147a10c7770f9c5a
successdevop <successraphael28@gmail.com>
2026-04-21 08:45:39 +0100

    fixed the error in my logger function, initially overwriting every log but now working as designed

--- src/hr_system/storage/json_operator.py
+++ src/hr_system/storage/json_operator.py
@@ -2,11 +2,18 @@ import json
 from datetime import datetime
 
 
-def save_to_jason_database(data, filename):
-    with open(filename, mode="w", encoding="utf-8") as file_writer:
-        json.dump(data, file_writer, indent=4)
+def save_to_json_database(data, filename):
+    try:
+        with open(filename, mode="w", encoding="utf-8") as file_writer:
+            json.dump(data, file_writer, indent=4)
+    except Exception as e:
+        print(f"Error saving data: {e}")
 
 
 def logger(msg, filename):
-    with open(filename, mode="w", encoding="utf-8") as file_writer:
-        file_writer.write(f"[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}]: {msg}\n")
+    try:
+        with open(filename, mode="a", encoding="utf-8") as file_writer:
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            file_writer.write(f"[{timestamp}]: {msg} \n")
+    except Exception as e:
+        print(f"ERROR writing to log file {filename}: {e}")
