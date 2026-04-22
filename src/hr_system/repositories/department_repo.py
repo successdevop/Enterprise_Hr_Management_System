4794cacf8e57dd18430fee79c789ac564756403a
successdevop <successraphael28@gmail.com>
2026-04-22 08:05:43 +0100

    implemented the department repository

--- src/hr_system/repositories/department_repo.py
+++ src/hr_system/repositories/department_repo.py
@@ -1,8 +1,9 @@
 import json
-from typing import Dict
+from typing import Dict, Optional, List
 from src.hr_system.models.department import Department
 from src.hr_system.models.employee import Employee
 from src.hr_system.storage.logger import Logger
+from src.hr_system.utils.exceptions import NotFoundError
 
 
 class DepartmentRepo:
@@ -10,6 +11,35 @@ class DepartmentRepo:
         self._dept_json_database = storage_file
         self._dept_database: Dict[str, Department] = {}
         self._load_dept_database()
+
+    def save_department(self, dept: Department):
+        self._dept_database[dept.name] = dept
+
+        savable_data = {
+            name: dept.to_dict()
+            for name, dept in self._dept_database.items()
+        }
+
+        try:
+            with open(self._dept_json_database, mode="w", encoding="utf-8") as file_writer:
+                json.dump(savable_data, file_writer, indent=4)
+        except Exception as e:
+            Logger.error(f"Error saving department | {e}")
+
+    def get_department_by_name(self, dept_name: str) -> Optional[Department]:
+        return self._dept_database.get(dept_name)
+
+    def get_department_by_manager(self, manager: Employee):
+        for dept in self._dept_database.values():
+            if dept.manager == manager.role:
+                return dept
+        raise NotFoundError("Manager not found")
+
+    def get_all_dept(self) -> List[Department]:
+        return list(self._dept_database.values())
+
+    def count_dept(self) -> int:
+        return len(self._dept_database)
 
     def _load_dept_database(self):
         self._dept_database.clear()
@@ -21,7 +51,10 @@ class DepartmentRepo:
                 if isinstance(data, dict):
                     for name, department in data.items():
                         if isinstance(department, dict):
-                            pass
+                            dept = Department.from_to_dict(department)
+                        else:
+                            dept = department
+                        self._dept_database[name] = dept
 
         except FileNotFoundError:
             # First run - File doesn't exist yet
@@ -34,5 +67,3 @@ class DepartmentRepo:
         except Exception as e:
             Logger.error(f"Error loading database: {e}")
             self._dept_database = {}
-
-
