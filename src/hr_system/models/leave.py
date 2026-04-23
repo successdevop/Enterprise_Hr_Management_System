85cec2dd75ff2e628e0c1588160307a31451abe5
successdevop <successraphael28@gmail.com>
2026-04-23 17:47:58 +0100

    implemented the leaveRequest Enum class and LeaveStatus class

---
+++ src/hr_system/models/leave.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from enum import Enum
+from src.hr_system.models.employee import Employee
+from src.hr_system.utils.exceptions import ValidationError
+
+
+class LeaveStatus(Enum):
+    PENDING = "Pending"
+    APPROVED = "Approved"
+    REJECTED = "Rejected"
+
+
+class LeaveRequest:
+    def __init__(self, employee: Employee, days: int):
+        if days <= 0:
+            raise ValidationError("Leave days must be greater than 0")
+
+        self.days = days
+        self.employee = employee
+        self.status = LeaveStatus.PENDING
+        self.created_at = datetime.now()
+        self.reviewed_by = None
+
+    def approve_leave(self, manager: Employee):
+        if self.status != LeaveStatus.PENDING:
+            raise ValidationError("Leave request already processed")
+
+        self.status = LeaveStatus.APPROVED
+        self.reviewed_by = {manager.name, manager.role}
+
+    def reject_leave(self, manager: Employee):
+        if self.status != LeaveStatus.PENDING:
+            raise ValidationError("Leave request already processed")
+
+        self.status = LeaveStatus.REJECTED
+        self.reviewed_by = {manager.name, manager.role}
+
+    def __repr__(self):
+        return f"<LeaveRequest name:{self.employee.name} | days_of_leave:{self.days} | status:{self.status.value}>"
