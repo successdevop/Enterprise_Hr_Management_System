9e950917101fe445a26a3dd1d8435bdc6877a845
successdevop <successraphael28@gmail.com>
2026-04-25 16:18:11 +0100

    implemented the salary strategy for Fulltime and contract employees

---
+++ src/hr_system/strategy/salary.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+
+class SalaryStrategy(ABC):
+    @abstractmethod
+    def calculate(self, base_salary):
+        pass
+
+
+class FullTimeStrategy(SalaryStrategy):
+    def calculate(self, base_salary):
+        tax = base_salary * 0.2
+        return base_salary, tax
+
+
+class ContractStrategy(SalaryStrategy):
+    def calculate(self, base_salary):
+        return base_salary, 0
